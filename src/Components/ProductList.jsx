2f4e7f0d0a893be8ba6d7eb26096f614896ac7ca
Covaar <co.va.ard@gmail.com>
2024-12-09 06:28:25 -0500

    Initial commit

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import './ProductList.css'; 
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from './CartSlice';
+import './ProductList.css'; // Importar archivo CSS para estilos específicos del componente
 
 const ProductList = () => {
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]); // Estado para almacenar productos deshabilitados
 
   const products = [
-    { id: 1, name: 'Product A', price: 60 },
-    { id: 2, name: 'Product B', price: 75 },
-    { id: 3, name: 'Product C', price: 30 },
+    { id: 1, name: 'Producto A', price: 60 },
+    { id: 2, name: 'Producto B', price: 75 },
+    { id: 3, name: 'Producto C', price: 30 },
   ];
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts, product.id]); // Marcar el producto como deshabilitado
+  };
 
   return (
     <div className="product-list">
-      <h2 className="product-list-title">Products</h2>
+      <h2 className="product-list-title">Productos</h2>
       <ul className="product-list-items">
-     
+        {products.map(product => (
+          <li key={product.id} className="product-list-item">
+            <span>{product.name} - ${product.price}</span>
+            <button 
+              className={`add-to-cart-btn ${disabledProducts.includes(product.id) ? 'disabled' : ''}`} 
+              onClick={() => handleAddToCart(product)}
+              disabled={disabledProducts.includes(product.id)} // Deshabilitar botón si el producto está en disabledProducts
+            >
+              Agregar al carrito
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
